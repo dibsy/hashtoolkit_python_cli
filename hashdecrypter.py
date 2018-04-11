a8baac44a7114bf0116b8ce41879f544abd81d1d
Arjun Singh Brar <arjun@outlook.ie>
2018-04-11 22:08:14 +0800

    added try-except for hashes not found

--- hashdecrypter.py
+++ hashdecrypter.py
@@ -8,16 +8,18 @@ if len(sys.argv)!=2:
 	print "usage : ./hashdecrypter.py 21232f297a57a5a743894a0e4a801fc3"
 else:
 
-	url = "http://hashtoolkit.com/reverse-hash?hash="+sys.argv[1]
+	url = "http://hashtoolkit.com/reverse-hash?hash={}".format(sys.argv[1])
 	request = requests.get(url)
 	data = request.text
 	soup = BeautifulSoup(data,'html.parser')
 
 	table = soup.find('table')
-	table_rows = table.find_all('tr')
+	try: 
+		table_rows = table.find_all('tr')
 
-	for tr in table_rows:
-		td = tr.find_all('td')
-		row = [i.text.strip() for i in td]
-		print str(row).encode('utf-8')
-		
+		for tr in table_rows:
+			td = tr.find_all('td')
+			row = [i.text.strip() for i in td]
+			print str(row).encode('utf-8')
+	except:
+		print("No hash found for {}".format(sys.argv[1]))
